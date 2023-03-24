50984b3e416571bbe811261c14143f0bfb69f0df
nagarajalva5790 <nagarajalva5790@gmail.com>
2023-03-24 16:03:11 +0530

    app js file change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Router, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './containers/Header';
+import ProductDetails from './containers/ProductDetails';
+import ProductLists from './containers/ProductLists';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello Word</h1>
+     <Router>
+      <Header/>
+      <Switch>
+          <Route path='/' exact component={ProductLists}/>
+          <Route path='/product/:productId' component={ProductDetails}/>
+          <Route>404 Not Found</Route>
+        </Switch>
+     </Router>
     </div>
   );
 }
---
+++ src/containers/ProductLists.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProductLists() {
+  return (
+    <div>ProductLists</div>
+  )
+}
